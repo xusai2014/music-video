aefca53f71ee1487369ac417019dac63e12e3e03
jerryxu <jerryxu521@gmail.com>
2022-01-09 19:05:20 +0800

    0.0.1

--- start.sh
+++ start.sh
@@ -1,2 +1,2 @@
-docker build --build-arg BUILD_VERSION=0.0.0 -t music-video .
-docker run --name video  -p 5000:5000 -v /data:/data -d music-video:latest --build-args BUILD_VERSION=0.0.0
+docker build --build-arg BUILD_VERSION=0.0.1 -t music-video .
+docker run --name video  -p 5000:5000 -v /data:/data -d music-video:latest --build-args BUILD_VERSION=0.0.1
