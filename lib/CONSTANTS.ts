207e567c052d1905a5c075a4cafd3876280bb569
Jerry <18800102517@163.com>
2023-03-27 16:20:54 +0800

    全站替换域名

--- lib/CONSTANTS.ts
+++ lib/CONSTANTS.ts
@@ -1,2 +1,2 @@
-export const BASE_URL: string = process.env.NODE_ENV === 'development' ? 'https://www.chejj.cc' : 'https://www.chejj.cc';
+export const BASE_URL: string = process.env.NODE_ENV === 'development' ? 'https://www.flygpt.cc' : 'https://www.flygpt.cc';
 
